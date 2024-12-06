7dad4efdde3978b58d44e2b6b1a4f440db172376
anonnymous256 <af083008@gmail.com>
2024-12-06 07:41:22 -0300

    erro no certificado

--- credentials.js
+++ credentials.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
+console.log('Caminho do certificado:', process.env.EFI_PAY_CERTIFICATE);
+
 module.exports = {
     sandbox: process.env.EFI_PAY_DEVELOPMENT === 'true',
     client_id: process.env.EFI_PAY_CLIENT_ID,
